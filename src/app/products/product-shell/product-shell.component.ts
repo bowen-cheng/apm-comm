da122fee0d4e376ed16e1579edfa653ed6800715
Bowen Cheng <blueicycky@gmail.com>
2019-01-26 14:44:22 -0500

    Add a 2nd subscriber to show how notifications are multicasted

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IProduct } from '../product';
+import { ProductService } from '../product.service';
+
 @Component({
   templateUrl: './product-shell.component.html'
 })
 export class ProductShellComponent implements OnInit {
-  pageTitle: string = 'Products';
-  monthCount: number;
+  protected pageTitle: string = 'Products';
+  protected monthCount: number;
 
-  constructor() {
-  }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.productChanges$.subscribe(
+      (selectedProduct: IProduct | null) => {
+        if (selectedProduct) {
+          const releaseDate = new Date(selectedProduct.releaseDate);
+          const now = new Date();
+          this.monthCount = (now.getMonth() - releaseDate.getMonth())
+            + 12 * (now.getFullYear() - releaseDate.getFullYear());
+        } else {
+          this.monthCount = 0;
+        }
+      });
   }
 
 }
